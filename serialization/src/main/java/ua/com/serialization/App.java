71560922aec24a3dc90564378eb079086aa7d9a4
Oleksandr Kruhlov <alkruglov777@gmail.com>
2020-02-07 09:38:40 +0200

    Make file path refactoring

--- serialization/src/main/java/ua/com/serialization/App.java
+++ serialization/src/main/java/ua/com/serialization/App.java
@@ -2,14 +2,14 @@ package ua.com.serialization;
 
 public class App {
 
-    // File name in Linux format, Windows format is "c\\\\"
+    // File name in Linux format, Windows format is "d:\\employee.ser"
     private static final String FILE_NAME = "/home/alex/Desktop/employee.ser";
 
     public static void main(String[] args) {
         SerializationUtils<Employee> employeeSerializationUtils = new SerializationUtils<>();
         Employee employee = new Employee("Antony", "Hopkins", 7000000);
-        employeeSerializationUtils.encodeAndSaveToFile("/home/alex/Desktop/employee.ser", employee);
-//        Employee employee = (Employee) employeeSerializationUtils.loadFromFileAndDecode("/home/alex/Desktop/employee.ser");
+        employeeSerializationUtils.encodeAndSaveToFile(FILE_NAME, employee);
+//        Employee employee = (Employee) employeeSerializationUtils.loadFromFileAndDecode(FILE_NAME);
 //        System.out.println(employee);
     }
 }
